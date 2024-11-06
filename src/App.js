ed9eea2cd627b868f36338ff9bbec5837e979f4f
Rishmita Tamuli <rishmitatamuli@Rishmitas-MacBook-Air.local>
2024-11-07 00:05:16 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <NavBar />
+     <NavBar />
       <Banner />
       <Skills />
       <Projects />
